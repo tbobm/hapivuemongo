c3002e4782fb30caa6304c798faafd047ff1b6f0
Theo Massard <theo.massard@1000mercis.com>
2018-09-12 21:23:17 +0200

    add base hapi route

---
+++ api/app.js
@@ -0,0 +1,30 @@
+'use strict';
+const Hapi = require('hapi');
+
+
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8000,
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function(request, h) {
+        return 'hello world';
+    }
+});
+
+async function start() {
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    };
+    console.log('server running at:', server.info.uri);
+};
+
+start();
