5524688224725b351480e6c521cd4827f096898d
Unknown <antoine@martin-julien.fr>
2018-09-18 11:59:15 +0200

    Follow teacher movies

--- api/app.js
+++ api/app.js
@@ -1,30 +1,68 @@
 'use strict';
 const Hapi = require('hapi');
+const Config = require('config');
 
+const options = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ log: '*', response: '*' }]
+        }, {
+            module: 'good-console'
+        }, 'stdout']
+    }
+};
 
 const server = Hapi.server({
-    host: 'localhost',
-    port: 8000,
+    host: Config.get('api.host'),
+    port: Config.get('api.port'),
 });
 
+const getEtna = (request, h) => {
+    return request.params
+};
+
+const postEtna = (request, h) => {
+    return request.payload
+};
 
 server.route({
     method: 'GET',
-    path: '/hello',
-    handler: function(request, h) {
-        return 'hello world';
-    }
+    path: '/etna/{etnaMessage?}',
+    handler: getEtna
 });
+
+server.route({
+    method: ['PUT', 'POST'],
+    path: '/etna',
+    config: {
+        payload: {
+            parse: true
+        }
+    },
+    handler: postEtna
+});
+
+
 
 async function start() {
     try {
+        await server.register({
+            plugin: require('good'),
+            options,
+        });
+
         await server.start();
     }
     catch (err) {
         console.log(err);
         process.exit(1);
     };
-    console.log('server running at:', server.info.uri);
+    await server.log('info', 'server running at: '+ server.info.uri);
 };
 
 start();
