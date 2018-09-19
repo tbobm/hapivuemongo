fd1566557040426c18948d14fe4fcb5e74d837d9
Unknown <antoine@martin-julien.fr>
2018-09-19 13:16:13 +0200

    prepare vue

---
+++ front/src/_nav.js
@@ -0,0 +1,19 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Crimes',
+      url: '/crimes',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Users',
+      url: '/users',
+      icon: 'icon-speedometer'
+    },
+  ]
+}
