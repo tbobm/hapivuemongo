9b62c7d22d5a423cf71bbd60b23a322e359e4bb2
Theo Massard <theo.massard@1000mercis.com>
2018-09-26 21:42:05 +0200

    add SQL_URI

---
+++ microservices/register/app.py
@@ -0,0 +1,15 @@
+import os
+
+import pymongo
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({'ok': True})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
