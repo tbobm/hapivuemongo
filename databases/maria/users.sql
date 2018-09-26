16fae62062b6fbe9d6f72ed0d470788c49a34b0f
Theo Massard <theo.massard@1000mercis.com>
2018-09-27 00:37:42 +0200

    Actually insert data

--- databases/maria/users.sql
+++ databases/maria/users.sql
@@ -12,6 +12,18 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL auto_increment,
     grade_id INT NOT NULL,
     active BOOLEAN NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    token VARCHAR(255),
     PRIMARY KEY(id),
     FOREIGN KEY(grade_id) REFERENCES grades(id)
 );
+
+INSERT INTO grades (`name`)
+    VALUES ('Agent');
+
+INSERT INTO grades (`name`)
+    VALUES ('Détective');
+
+INSERT INTO grades (`name`)
+    VALUES ('Chef de la police');
