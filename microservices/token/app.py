691bfdb51c6d87bb97b55a8fb55995fcae3a37d8
Theo Massard <theo.massard@1000mercis.com>
2018-10-01 19:51:53 +0200

    ms: token microservice
    Closes #27

---
+++ microservices/token/app.py
@@ -0,0 +1,69 @@
+"""
+Tiny microservice registering users based on flask.
+"""
+import os
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQL_URI')
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    """Base Users."""
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean)
+    grade_id = db.Column(db.Integer, db.ForeignKey('grades.id'))
+    username = db.Column(db.String(255), nullable=False)
+    token = db.Column(db.String(255))
+    grade = db.relationship('Grades')
+
+
+class Grades(db.Model):
+    """Base Users."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    p_read = db.Column(db.Boolean)
+    p_edit = db.Column(db.Boolean)
+    p_add = db.Column(db.Boolean)
+    p_delete = db.Column(db.Boolean)
+    p_validate = db.Column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    return jsonify({'ok': True})
+
+
+@app.route('/token', methods=['POST'])
+def token_user():
+    data = request.get_json()
+    token = data.get('token')
+    user = Users.query.filter_by(
+        token=token,
+    ).first()
+    if user is None:
+        return jsonify({'error': True}), 404
+    response = {
+        'error': False,
+        'permissions': {
+            'read': user.grade.p_read,
+            'add': user.grade.p_add,
+            'edit': user.grade.p_edit,
+            'validate': user.grade.p_validate,
+            'delete': user.grade.p_delete,
+        },
+        'user': {
+            'username': user.username,
+            'grade': user.grade.name,
+        },
+        'token': user.token,
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5002)
