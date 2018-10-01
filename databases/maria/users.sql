7e79c7fc61f93e7009810217660d24a598bc3d09
Theo Massard <theo.massard@1000mercis.com>
2018-10-01 11:33:24 +0200

    sql: implement permissions
    Closes #34

--- databases/maria/users.sql
+++ databases/maria/users.sql
@@ -5,6 +5,11 @@ DROP TABLE IF EXISTS grades;
 CREATE TABLE grades (
     id INT NOT NULL auto_increment,
     name ENUM('Agent', 'Détective', 'Chef de la police'),
+    p_read BOOLEAN NOT NULL,
+    p_add BOOLEAN NOT NULL,
+    p_edit BOOLEAN NOT NULL,
+    p_delete BOOLEAN NOT NULL,
+    p_validate BOOLEAN NOT NULL,
     CONSTRAINT grades_pk
     PRIMARY KEY(id)
 );
@@ -21,11 +26,32 @@ CREATE TABLE users (
     FOREIGN KEY(grade_id) REFERENCES grades(id)
 );
 
-INSERT INTO grades (`name`)
-    VALUES ('Agent');
+INSERT INTO grades (`name`, `p_read`, `p_add`, `p_edit`, `p_delete`, `p_validate`)
+    VALUES (
+        'Agent',
+        true,
+        false,
+        false,
+        false,
+        false
+);
 
-INSERT INTO grades (`name`)
-    VALUES ('Détective');
+INSERT INTO grades (`name`, `p_read`, `p_add`, `p_edit`, `p_delete`, `p_validate`)
+    VALUES (
+        'Détective',
+        true,
+        true,
+        true,
+        false,
+        false
+);
 
-INSERT INTO grades (`name`)
-    VALUES ('Chef de la police');
+INSERT INTO grades (`name`, `p_read`, `p_add`, `p_edit`, `p_delete`, `p_validate`)
+    VALUES (
+        'Chef de la police',
+        true,
+        true,
+        true,
+        true,
+        true
+);
