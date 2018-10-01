68c4754e148b8b0e1abd4e483cd44bd506a0b4ca
Theo Massard <theo.massard@1000mercis.com>
2018-10-01 18:20:16 +0200

    ms: connection microservice available through endpoint /connectUser
    Closes #26

---
+++ microservices/connection/app.py
@@ -0,0 +1,75 @@
+"""
+Tiny microservice managing connection based on Flask.
+"""
+import os
+
+from secrets import token_urlsafe
+
+from flask import Flask, jsonify, request
+from pymysql.err import IntegrityError
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQL_URI')
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    """Base Users."""
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean)
+    grade_id = db.Column(db.Integer, db.ForeignKey('grades.id'))
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    token = db.Column(db.String(255))
+    grade = db.relationship('Grades')
+
+
+class Grades(db.Model):
+    """Base Users."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    p_read = db.Column(db.Boolean)
+    p_edit = db.Column(db.Boolean)
+    p_add = db.Column(db.Boolean)
+    p_delete = db.Column(db.Boolean)
+    p_validate = db.Column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    return jsonify({'ok': True})
+
+
+@app.route('/connectUser', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    user = Users.query.filter_by(
+        username=username,
+        password=password,
+    ).first()
+    if not user:
+        return jsonify({'error': True}), 404
+    token = token_urlsafe(64)
+    user.token = token
+    db.session.add(user)
+    db.session.commit()
+    response = {
+        'error': False,
+        'permissions': {
+            'read': user.grade.p_read,
+            'add': user.grade.p_add,
+            'edit': user.grade.p_edit,
+            'validate': user.grade.p_validate,
+            'delete': user.grade.p_delete,
+        },
+        'token': user.token,
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
