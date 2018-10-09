60c89738a94cd4b58768cbfbc725a9ca1b62eab2
Theo Massard <theo.massard@1000mercis.com>
2018-10-09 22:00:43 +0200

    ms: add Delete microservice
    Closes #30

---
+++ microservices/delete/app.py
@@ -0,0 +1,45 @@
+"""
+Tiny microservice managing connection based on Flask.
+"""
+import os
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQL_URI')
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    """Base Users."""
+    id = db.Column(db.Integer, primary_key=True)
+
+
+@app.route('/')
+def index():
+    return jsonify({'ok': True})
+
+
+@app.route('/deleteUser', methods=['DELETE'])
+def register_user():
+    user = Users.query.filter_by(
+        active=False,
+    ).first()
+
+    if user is None:
+        response = {
+            'error': True,
+        }
+        return jsonify(response), 404
+
+    db.session.delete(user)
+    response = {
+        'error': False,
+    }
+    return jsonify(response), 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003)
