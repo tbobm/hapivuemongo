f0cfa5345fb6cc24fddce30c463c28287c8fbd2b
Theo Massard <theo.massard@1000mercis.com>
2018-10-09 22:22:02 +0200

    modb: add indexes
    Closes #13

--- databases/mongo-seed/csv_importer.py
+++ databases/mongo-seed/csv_importer.py
@@ -11,6 +11,37 @@ import arrow
 from pymongo import MongoClient, InsertOne
 
 
+def ignore(f):
+    return f
+
+
+def get_datetime(value):
+    return arrow.get(value).datetime
+
+
+FORMATTER = {
+    'compnos': literal_eval,
+    'naturecode': ignore,
+    'incident_type_description': ignore,
+    'main_crimecode': ignore,
+    'reptdistrict': ignore,
+    'reportingarea': literal_eval,
+    'fromdate': get_datetime,
+    'weapontype': ignore,
+    'shooting': literal_eval,
+    'domestic': literal_eval,
+    'shift': ignore,
+    'year': literal_eval,
+    'month': literal_eval,
+    'day_week': ignore,
+    'ucrpart': ignore,
+    'x': literal_eval,
+    'y': literal_eval,
+    'streetname': ignore,
+    'xstreetname': ignore,
+    'location': literal_eval,
+}
+
 FILE = os.environ.get('CSV_FILE', 'data.csv')
 DATABASE = MongoClient(
     os.environ.get(
@@ -21,38 +52,8 @@ DATABASE = MongoClient(
 COLLECTION = 'crimes'
 
 
-def ignore(f):
-    return f
-
-
-def get_datetime(value):
-    return arrow.get(value).datetime
-
-
 def format_row(row):
-    formatter = {
-        'compnos': literal_eval,
-        'naturecode': ignore,
-        'incident_type_description': ignore,
-        'main_crimecode': ignore,
-        'reptdistrict': ignore,
-        'reportingarea': literal_eval,
-        'fromdate': get_datetime,
-        'weapontype': ignore,
-        'shooting': literal_eval,
-        'domestic': literal_eval,
-        'shift': ignore,
-        'year': literal_eval,
-        'month': literal_eval,
-        'day_week': ignore,
-        'ucrpart': ignore,
-        'x': literal_eval,
-        'y': literal_eval,
-        'streetname': ignore,
-        'xstreetname': ignore,
-        'location': literal_eval,
-    }
-    return {k: formatter[k](v) for k, v in row.items()}
+    return {k: FORMATTER[k](v) for k, v in row.items()}
 
 
 def get_rows():
@@ -66,11 +67,17 @@ def prepare_row(row):
     return InsertOne(row)
 
 
+def create_indexes(collection):
+    for field in FORMATTER.keys():
+        collection.create_index(field)
+
+
 def main():
     print('processing rows')
     rows = get_rows()
     res = DATABASE[COLLECTION].bulk_write([prepare_row(row) for row in rows])
     print(res)
+    create_indexes(DATABASE[COLLECTION])
 
 
 if __name__ == '__main__':
